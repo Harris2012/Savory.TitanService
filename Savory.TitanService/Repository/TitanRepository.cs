6dbd27c14fbef309156c9939d750df69476d6d86
HarrisZhang <harriszhang@live.cn>
2018-01-31 22:38:57 +0800

    大迁移

---
+++ Savory.TitanService/Repository/TitanRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savory.TitanService.Repository
+{
+    public static class TitanRepository
+    {
+        public static string GetConnString(string dbName)
+        {
+            string returnValue = null;
+
+            using (SqlConnection sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["SavoryTitanDB"].ConnectionString))
+            {
+                using (SqlCommand sqlCmd = new SqlCommand())
+                {
+                    sqlCmd.CommandText = "SELECT ConnString FROM ConnStringTable WHERE DBName = @DBName";
+                    sqlCmd.Connection = sqlConn;
+                    sqlCmd.Parameters.AddWithValue("DBName", dbName);
+
+                    sqlConn.Open();
+
+                    var reader = sqlCmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        returnValue = reader["ConnString"].ToString();
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ Savory.TitanService/TitanService.cs
@@ -0,0 +1,57 @@
+﻿using Savory.TitanService.Contract;
+using Savory.TitanService.Repository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savory.TitanService
+{
+    public class TitanService : ServiceBase, ITitanService
+    {
+        ServiceHost host = null;
+
+        #region ServiceBase Members
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                host = new ServiceHost(this.GetType());
+
+                host.Open();
+            }
+            catch (Exception ex)
+            {
+                string fileName = string.Format("OnStartException_{0:yyyyMMddHHmmss}", DateTime.Now);
+
+                File.WriteAllText(fileName, ex.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                string fileName = string.Format("OnStopException_{0:yyyyMMddHHmmss}", DateTime.Now);
+
+                File.WriteAllText(fileName, ex.ToString());
+            }
+        }
+        #endregion
+
+        #region ITitanService Members
+        public string GetConnString(string dbName)
+        {
+            return TitanRepository.GetConnString(dbName);
+        }
+        #endregion
+    }
+}
